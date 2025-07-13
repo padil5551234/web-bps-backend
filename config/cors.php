94be3bd013b355137cc94a2c12a967f037d8acc1
Padil M Zaki <padilzaki73@gmail.com>
2025-07-13 12:09:58 +0700

    change lagi

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [ 
 'paths' => ['api/*'], 
 'allowed_methods' => ['*'], 
-'allowed_origins' => ['https://web-bps-frontend-psi.vercel.app','http://localhost:5173'], 
+'allowed_origins' => ['https://web-bps-frontend-psi.vercel.app'], 
 'allowed_origins_patterns' => [], 
 'allowed_headers' => ['*'], 
 'exposed_headers' => [], 
